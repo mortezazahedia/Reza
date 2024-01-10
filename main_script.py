f7fd096ae90d248cb56ebd3eb5c407f07427fb0c
mortezazahedia <morteza.zahedi.a@gmail.com>
2024-01-10 08:34:42 +0100

    d

--- main_script.py
+++ main_script.py
@@ -1,5 +1,6 @@
 # main_script.py
 from my_package.calculator import Calculator
+import os
 
 def main():
     # Perform addition using the Calculator class
@@ -7,7 +8,8 @@ def main():
     result = calc.add(2, 3)
 
     # Save the result in result.txt
-    with open("result.txt", "w") as f:
+    file_path = os.path.join(os.getcwd(), "result.txt")
+    with open(file_path, "w") as f:
         f.write(str(result))
 
 if __name__ == "__main__":
